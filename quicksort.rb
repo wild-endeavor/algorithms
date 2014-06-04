bcc84f485d256c157660fe1d981de05bea57e4e0
kaylindris <yt7739@gmail.com>
2014-06-04 12:17:20 -0700

    updating comments in quicksort

--- quicksort.rb
+++ quicksort.rb
@@ -19,8 +19,6 @@ class Array
     end
     
     middle_val = self[middle_idx]
-    # p self[left..right]
-    # puts "Left: #{left_val} Middle: #{middle_val} Right: #{right_val}"
 
     return middle_idx if left_val <= middle_val && middle_val <= right_val
     return middle_idx if right_val <= middle_val && middle_val <= left_val
@@ -32,6 +30,7 @@ class Array
     return right if middle_val <= right_val && right_val <= left_val
   end
 end
+
 
 input = [3, 10, 6, 4, 1, 5, 7, 9, 2, 8]
 
@@ -50,11 +49,10 @@ end
 
 # input = read_input("./sample")
 input = read_input("./quicksort_input.txt")
-# p input.length
 
+# This partition method needs a bit of work... it was meant to comply _exactly_ with
+# the process that was advised in the coursera roughgarden class.
 def partition(array, left, right)
-  # puts "\n"
-  # p "*******Array: #{array} with L: #{left} R: #{right}"
   if right - left == 0
     return true
   end
@@ -66,12 +64,10 @@ def partition(array, left, right)
   # pivot_idx = left + rand(right - left + 1) # random
   if right - left >= 2          # 144100
     pivot_idx = array.median_of_three(left, right)
-    # puts " Picking #{pivot_idx}"
   else
     pivot_idx = right
   end
 
-  # puts "  Swapping pivot index #{pivot_idx} value #{array[pivot_idx]}"
   array.swap(left, pivot_idx)
   pivot_idx = left
   pivot_value = array[left]
@@ -81,16 +77,12 @@ def partition(array, left, right)
 
   until (jj > right)
 
-    # puts "========== #{jj} ========"
-    # p "    #{array}"
     if array[jj] >= pivot_value && !have_seen_greater
       have_seen_greater = true
       ii = jj
-      # puts "  Setting greater than element #{ii} value #{array[ii]}"
     end
 
     if array[jj] < pivot_value && have_seen_greater
-      # puts "  Swapping #{ii} and #{jj} values #{array[ii]}, #{array[jj]}"
       array.swap(ii, jj)
       ii += 1
     end
@@ -98,58 +90,23 @@ def partition(array, left, right)
   end
 
   if have_seen_greater
-    # puts "Seen greater than pivot - swapping #{pivot_idx} with #{ii - 1}"
     array.swap(pivot_idx, ii - 1)
     return ii - 1
   else
-    # puts "Did not see greater than pivot, right is #{right}"
     array.swap(left, right)
     return right
   end
 end
 
-
-# array = [3, 1, 2, 5, 1]
-# p partition(array, 0, array.length - 1)
-# p "Result: #{array}"
-
-# array = [3, 1]
-# p partition(array, 0, array.length - 1)
-# p "Result: #{array}"
-
-# array = [1, 3]
-# p partition(array, 0, array.length - 1)
-# p "Result: #{array}"
-
-# array = [3]
-# p partition(array, 0, array.length - 1)
-# p "Result: #{array}"
-
-# array = [6, 5, 1, 1, 5, 6]
-# p partition(array, 0, array.length - 1)
-# p "Result: #{array}"
-
-# array = [3, 1, 1, 1, 3]
-# p partition(array, 0, array.length - 1)
-# p "Result: #{array}"
-
-# partition(input, 0, 9)
-
 $temp = 0
 
 def quicksort(array, left, right)
-  # print "Sorting lidx: #{left} ridx: #{right}"
-  # puts "" if right - left == 0
 
   return if right - left == 0
   raise "Error" if right - left < 0
   $temp += right - left
 
   pivot_idx = partition(array, left, right)
-  # p "After partition #{array} with pivot index #{pivot_idx}"
-
-  # puts "**   1st recursive call: Left: #{left}, #{pivot_idx - 1}"
-  # puts "**   2nd recursive call: Left: #{pivot_idx + 1}, #{right}"
 
   quicksort(array, left, pivot_idx - 1) if pivot_idx > left
   quicksort(array, pivot_idx + 1, right) if pivot_idx < right
