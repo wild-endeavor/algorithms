e89adc7dd13b199c04d80a0637508215371f9748
kaylindris <yt7739@gmail.com>
2014-06-29 22:36:35 -0700

    finished basic heap implementation, fixed some bugs, added the code for programming assignment 6

--- heap.rb
+++ heap.rb
@@ -19,21 +19,33 @@ class Heap
 
   def parent_index(i)
     raise "Heap error: already root" if i == 0
-    i / 2
+    (i - 1) / 2
   end
 
   # TODO: consider whether these should be class methods
   def left_child_idx(i)
-    2 * i
+    2 * i + 1
   end
 
   def right_child_idx(i)
-    2 * i + 1
+    2 * i + 2
   end
 
   def insert(value)
     self.heap << value
     self.bubble_up
+  end
+
+  def check
+    i = 0
+    until left_child_idx(i) > self.heap.length - 1
+      p self.heap if self.heap[left_child_idx(i)] < self.heap[i]
+      raise "bad heap left" if self.heap[left_child_idx(i)] < self.heap[i]
+      if right_child_idx(i) < self.heap.length
+        raise "bad heap right" if self.heap[right_child_idx(i)] < self.heap[i]
+      end
+      i += 1
+    end
   end
 
   # Currently these versions of bubble up and down work on the last and
@@ -51,11 +63,11 @@ class Heap
   def bubble_down
     return if self.heap.length == 0
     current = 0
-    while left_child_idx(current) < self.heap.length 
+    while left_child_idx(current) < self.heap.length
       # if the right child exists
+      left_value = self.heap[left_child_idx(current)]
       if right_child_idx(current) < self.heap.length
         right_value = self.heap[right_child_idx(current)]
-        left_value = self.heap[left_child_idx(current)]
         if self.heap[current] <= left_value && self.heap[current] <= right_value
           break
         elsif right_value < left_value
@@ -79,9 +91,14 @@ class Heap
   def extract_min
     root = self.heap[0]
     new_root = self.heap.pop
+    return new_root if self.heap.empty?
     self.heap[0] = new_root
     self.bubble_down
     root
+  end
+
+  def size
+    self.heap.length
   end
 
   def peek
@@ -90,8 +107,16 @@ class Heap
 
 end
 
-
 def test
+  h = Heap.new()
+  h.insert(6331)
+  h.insert(9290)
+  h.insert(2793)
+  h.insert(6195)
+  h.extract_min
+  h.insert(2793)
+  h.insert(5685)
+  h.extract_min
   h = Heap.new()
   h.insert(5)
   p h.heap
--- median_maintenance.rb
+++ median_maintenance.rb
@@ -1,5 +1,81 @@
 #!/usr/bin/env ruby
 
-# make two heaps, one min and one max
+require_relative "heap"
 
-# 
+class Maintainer
+  attr_accessor :smaller, :larger
+
+  def initialize
+    # make two heaps, one min and one max
+    # the smaller one is an extract-max hash
+    @smaller = Heap.new()
+    @larger = Heap.new()
+  end
+
+  def get_median
+    median = -self.smaller.peek
+  end
+
+  def insert(value)
+    largest_of_smaller = self.smaller.peek
+    if largest_of_smaller.nil? || value <= -1 * largest_of_smaller
+      self.smaller.insert(value * -1)
+    else
+      self.larger.insert(value)
+    end
+    self.rebalance
+    # self.display
+  end
+
+  # when rebalancing, if even, then make even
+  # if odd, then put the odd element into the smaller one
+  def rebalance
+    total = smaller.size + larger.size
+    if larger.size > smaller.size
+      while larger.size > smaller.size
+        ele = larger.extract_min
+        ele *= -1
+        smaller.insert(ele)
+      end
+    else
+      while smaller.size > larger.size + 1
+        ele = smaller.extract_min
+        ele *= -1
+        larger.insert(ele)
+
+      end
+    end
+  end
+
+  def display
+    left_half_max = -1 * smaller.peek
+    right_half_min = larger.peek
+    puts "L: #{smaller.size} / #{left_half_max} R: #{larger.size} / #{right_half_min}"
+  end
+
+end
+
+
+# if something comes in, see if it is bigger than the biggest thing in the smaller set,
+# if so, then put it into the bigger one, then rebalance.
+input_array = []
+File.open("median_input.txt", "r") do |file|
+  file.each_line do |line|
+    input_array << Integer(line.chomp)
+  end
+end
+
+med = Maintainer.new
+sum = 0
+while input_array.length > 0
+  current = input_array.shift
+  med.insert(current)
+  sum += med.get_median
+end
+med.display
+p sum
+p sum % 10000
+
+# 46831213
+# 1213
+
